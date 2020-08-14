21a9ef36d435fe83df31d1baf50693fff407cec7
Cool-Katt <69585372+Cool-Katt@users.noreply.github.com>
2020-08-14 15:11:17 +0300

    Adding ability to delete flashcards

--- src/main/java/sofuni/flashy/repositories/FlashcardRepository.java
+++ src/main/java/sofuni/flashy/repositories/FlashcardRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sofuni.flashy.models.entities.FlashcardEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface FlashcardRepository extends JpaRepository<FlashcardEntity, Long>
 {
+    Optional<FlashcardEntity> findByTitle(String title);
+
+    void deleteByTitle(String title);
 }
